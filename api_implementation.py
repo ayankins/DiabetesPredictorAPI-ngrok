b493e0f2fde9de24b86b3b4d486ca8948d57807d
Ayan Naeem Khan <172098573+ayankins@users.noreply.github.com>
2024-06-29 04:11:16 +0530

    Add files via upload

---
+++ api_implementation.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 04:22:51 2024
+
+@author: Helios
+"""
+
+import json
+import requests 
+
+url = 'https://81e5-35-188-255-95.ngrok-free.app/diabetes_prediction'
+
+input_data_for_model = {
+    
+    'Pregnancies' : 1,
+    'Glucose' : 85,
+    'BloodPressure' : 66,
+    'SkinThickness' : 29,
+    'Insulin' : 0,
+    'BMI' : 26.6,
+    'DiabetesPedigreeFunction' : 0.351,
+    'Age' : 31
+    
+    }
+
+input_json = json.dumps(input_data_for_model)
+
+response = requests.post(url, data=input_json)
+
+print(response.text)
